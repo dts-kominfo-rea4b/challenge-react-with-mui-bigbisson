b4f8fcae8468d049a618d2b825f74209c4756297
bigbisson <bigmaster.indra@gmail.com>
2022-09-27 18:56:56 +0700

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
-import React from 'react';
+import {
+  Typography
+} from '@mui/material'
+import React from "react";
+import "./Header.css";
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <>
+      <div className="headerApp">
+        <Typography variant='h4' className='title'>Call a Friend</Typography>
+        <Typography variant='subtitle1' className='sub-title'>Your friendly contact app</Typography>
+      </div>
+    </>
   );
 };
 
